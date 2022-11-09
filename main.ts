c0346ff4aa602f96979b0924dda213a0ffa987de
Ketan Tailor <ketan.tailor@live.co.uk>
2022-11-09 21:30:25 +0000

    Update

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@
 
 import { Server } from "https://deno.land/std@0.163.0/http/mod.ts";
 
-const port = 4505;
+const port = 8771;
 const handler = (request: Request) => {
     //const userAgent = request.headers.get("user-agent") ?? "Unknown";
     //const body = `Your user-agent is:\n\n${userAgent}`;
 
     //return new Response(body, { status: 200 });
-    return new Response("Hello, World!\n\nFrom Little Linux Server", { status: 200 });
+    return new Response("Hello, World!\n\nFrom Ubuntu, Deno & Little Linux Server", { status: 200 });
 };
 
 const server = new Server({ port, handler });
